2965bded177cbb5a7cac926136fe1858ddfa2328
Francis <francis-r-emond@hotmail.com>
2016-06-05 10:02:04 -0400

    Revert "Revert "benny""
    This reverts commit 61278610f98e5de5dc06e649e3e0651e54e4534a.

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -613,6 +613,12 @@ namespace SlackathonMTL
             float points = Matrix.GetPoints(person, subject);
 
             StringBuilder response = new StringBuilder();
+            if (points <= 0)
+            {
+                response.Append(string.Format(Reply.GetReply(ReplyType.NoPointCount).Text, person.Username, subject.Name));
+                return message.CreateReplyMessage(response.ToString(), "en");
+            }
+
             response.Append(string.Format(Reply.GetReply(ReplyType.PointCount).Text, person.Username, points, subject.Name));
             return message.CreateReplyMessage(response.ToString(), "en");
         }
