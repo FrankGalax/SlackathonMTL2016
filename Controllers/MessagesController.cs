9841a4994b4f4e598989c1862c4dad9e582b2f0d
tomSagala <tomsagala@gmail.com>
2016-06-05 06:56:07 -0400

    benny

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -99,8 +99,7 @@ namespace SlackathonMTL
                                 return message.CreateReplyMessage(Reply.GetReply(ReplyType.Thanks).Text);
                             case IntentType.Goodbyes:
                                 Message gg = message.CreateReplyMessage(Reply.GetReply(ReplyType.Goodbyes).Text);
-                                gg.From.IsBot = false;
-                                gg.Text = JsonConvert.SerializeObject(gg);
+                                gg.Text = JsonConvert.SerializeObject(gg.BotUserData);
                                 return gg;
                         }
                     }
